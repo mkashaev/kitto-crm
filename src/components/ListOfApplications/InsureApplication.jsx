2b87202b4cde3d09411a334d6aca8b396bfe94a6
Marat <kashaevmt@gmail.com>
2019-04-11 00:11:50 +0300

    list of coupons feature

--- src/components/ListOfApplications/InsureApplication.jsx
+++ src/components/ListOfApplications/InsureApplication.jsx
@@ -1,16 +1,76 @@
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import React from 'react'
+import { Button, Modal, Grid } from 'semantic-ui-react'
 
-const InsureApplicationModal = () => (
-  <Modal trigger={}>
-    <Modal.Header>Select a Photo</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
-      <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>We've found the following gravatar image associated with your e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
-      </Modal.Description>
+const InsureApplicationModal = ({open, close, data}) => (
+  <Modal open={open} onClose={close}>
+    {console.log(data)}
+    <Modal.Header>Insurance contract number: {data===''? '' : data.numberOfApp}</Modal.Header>
+    <Modal.Content>
+      <Grid columns={4}>
+        <Grid.Row>
+          <Grid.Column>
+            <p>Type of property:</p>
+            <p>City:</p>
+            <p>Street:</p>
+            <p>House number:</p>
+            <p>Appartment number:</p>
+            <p>Cost:</p>
+            <p>Date start of agreement:</p>
+            <p>Date end of the agreement:</p>
+            <p>Date of application:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>{data===''? '' : data.insureData.typeOfProperty}</p>
+            <p>{data===''? '' : data.insureData.city}</p>
+            <p>{data===''? '' : data.insureData.street}</p>
+            <p>{data===''? '' : data.insureData.houseNumber}</p>
+            <p>{data===''? '' : data.insureData.appartmentNumber}</p>
+            <p>{data===''? '' : data.insureData.cost}</p>
+            <p>{data===''? '' : data.insureData.dateStartOfAgreement}</p>
+            <p>{data===''? '' : data.insureData.dateEndOfTheAgreement}</p>
+            <p>{data===''? '' : data.insureData.dateOfApplication}</p>
+          </Grid.Column>
+
+          <Grid.Column>
+            <p>Name:</p>
+            <p>Surname:</p>
+            <p>Last name:</p>
+            <p>Passport seria:</p>
+            <p>Passport number:</p>
+            <p>Passport issue date:</p>
+            <p>Passport issued by:</p>
+            <p>Phone nubmer:</p>
+            <p>Email:</p>
+          </Grid.Column>
+          <Grid.Column>
+            <p>{data===''? '' : data.personalData.name}</p>
+            <p>{data===''? '' : data.personalData.surname}</p>
+            <p>{data===''? '' : data.personalData.lastName}</p>
+            <p>{data===''? '' : data.personalData.passportSeria}</p>
+            <p>{data===''? '' : data.personalData.passportNumber}</p>
+            <p>{data===''? '' : data.personalData.passportIssueDate}</p>
+            <p>{data===''? '' : data.personalData.passportIssuedBy}</p>
+            <p>{data===''? '' : data.personalData.phoneNubmer}</p>
+            <p>{data===''? '' : data.personalData.email}</p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Modal.Content>
+
+    <Modal.Actions>
+    <Button
+        negative
+        icon="close"
+        labelPosition="right"
+        content="Reject"
+      />
+      <Button
+        positive
+        icon="checkmark"
+        labelPosition="right"
+        content="Accept"
+      />
+    </Modal.Actions>
   </Modal>
 )
 
--- src/components/ListOfApplications/ListOfApplications.jsx
+++ src/components/ListOfApplications/ListOfApplications.jsx
@@ -1,123 +1,137 @@
 import React from "react";
-import { List, Modal, Grid, Button } from "semantic-ui-react";
+import { List, Segment, Header, Container } from "semantic-ui-react";
+import InsureApplicationModal from './InsureApplication'
 
+import './ListOfApplications.css'
+
+// Tmp data
+const data = [
+  {
+    numberOfApp: '010101',
+    insureData: {
+      'typeOfProperty': 'Appartment',
+      'city': 'Moscow',
+      'street': 'Bolshevikov street',
+      'houseNumber': '23',
+      'appartmentNumber': '104',
+      'cost': '10.000.000 Rub',
+      'dateStartOfAgreement': '10.12.2018',
+      'dateEndOfTheAgreement': '10.12.2019',
+      'dateOfApplication': '05.12.1018'
+    },
+    personalData: {
+      'name': 'Huan',
+      'surname': 'Andreas',
+      'lastName': 'Pereiro',
+      'passportSeria': '2025',
+      'passportNumber': '102030',
+      'passportIssueDate': '03.05.1990',
+      'passportIssuedBy': 'Department number: 10',
+      'phoneNubmer': '8-800-555-35-35',
+      'email': 'big_huan@gamil.com',
+    }
+  },
+  {
+    numberOfApp: '010102',
+    insureData: {
+      'typeOfProperty': 'Appartment',
+      'city': 'Moscow',
+      'street': 'Bolshevikov street',
+      'houseNumber': '23',
+      'appartmentNumber': '104',
+      'cost': '10.000.000 Rub',
+      'dateStartOfAgreement': '10.12.2018',
+      'dateEndOfTheAgreement': '10.12.2019',
+      'dateOfApplication': '05.12.1018'
+    },
+    personalData: {
+      'name': 'Huan',
+      'surname': 'Andreas',
+      'lastName': 'Pereiro',
+      'passportSeria': '2025',
+      'passportNumber': '102030',
+      'passportIssueDate': '03.05.1990',
+      'passportIssuedBy': 'Department number: 10',
+      'phoneNubmer': '8-800-555-35-35',
+      'email': 'big_huan@gamil.com',
+    }
+  },
+  {
+    numberOfApp: '010103',
+    insureData: {
+      'typeOfProperty': 'Appartment',
+      'city': 'Moscow',
+      'street': 'Bolshevikov street',
+      'houseNumber': '23',
+      'appartmentNumber': '104',
+      'cost': '10.000.000 Rub',
+      'dateStartOfAgreement': '10.12.2018',
+      'dateEndOfTheAgreement': '10.12.2019',
+      'dateOfApplication': '05.12.1018'
+    },
+    personalData: {
+      'name': 'Huan',
+      'surname': 'Andreas',
+      'lastName': 'Pereiro',
+      'passportSeria': '2025',
+      'passportNumber': '102030',
+      'passportIssueDate': '03.05.1990',
+      'passportIssuedBy': 'Department number: 10',
+      'phoneNubmer': '8-800-555-35-35',
+      'email': 'big_huan@gamil.com',
+    }
+  }
+]
 
 class ModalModalExample extends React.Component {
   state = {
-    open: false
+    open: false,
+    data: data,
+    currentApp: ''
   }
 
-  handleOnOpen = () => {
+  handleOnOpen = (app) => {
     this.setState({
-      open: true
+      open: true,
+      currentApp: app 
     })
   }
 
   handleOnClose = () => {
     this.setState({
-      open: false
+      open: false, 
+      currentApp: ''
     })
   }
 
   render() {
     return (
       <div>
-        <List divided relaxed>
-          <List.Item>
-            <List.Icon name="home" size="large" verticalAlign="middle" />
-            <List.Content>
-              <List.Header as="a" onClick={this.handleOnOpen}>Insurance contract number: 10</List.Header>
-              <List.Description as="a">Created: 10.10.2019</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name="building" size="large" verticalAlign="middle" />
-            <List.Content>
-              <List.Header as="a">Insurance contract number: 10</List.Header>
-              <List.Description as="a">Created: 10.10.2019</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name="home" size="large" verticalAlign="middle" />
-            <List.Content>
-              <List.Header as="a">Insurance contract number: 10</List.Header>
-              <List.Description as="a">Created: 10.10.2019</List.Description>
-            </List.Content>
-          </List.Item>
-        </List>
-
-        <Modal open={this.state.open} onClose={this.handleOnClose}>
-        <Modal.Header>Insurance contract number: {10}</Modal.Header>
-        <Modal.Content>
-          <Grid columns={4}>
-            <Grid.Row>
-              <Grid.Column>
-                <p>Tipe of property:</p>
-                <p>City:</p>
-                <p>Street:</p>
-                <p>House number:</p>
-                <p>Appartment number:</p>
-                <p>Cost:</p>
-                <p>Date start of agreement:</p>
-                <p>Date end of the agreement:</p>
-                <p>Date of application:</p>
-              </Grid.Column>
-              <Grid.Column>
-                <p>Appartment</p>
-                <p>Moscow</p>
-                <p>Bolshevikov street</p>
-                <p>23</p>
-                <p>104</p>
-                <p>10.000.000 Rub</p>
-                <p>10.12.2018</p>
-                <p>10.12.2019</p>
-                <p>05.12.1018</p>
-              </Grid.Column>
-
-              <Grid.Column>
-                <p>Name:</p>
-                <p>Surname:</p>
-                <p>Last name:</p>
-                <p>Passport seria:</p>
-                <p>Passport number:</p>
-                <p>Passport issue date:</p>
-                <p>Passport issued by:</p>
-                <p>Phone nubmer:</p>
-                <p>Email:</p>
-              </Grid.Column>
-              <Grid.Column>
-                <p>Huan</p>
-                <p>Andreas</p>
-                <p>Pereiro</p>
-                <p>2025</p>
-                <p>102030</p>
-                <p>03.05.1990</p>
-                <p>Department number: 10</p>
-                <p>8-800-555-35-35</p>
-                <p>big_huan@gamil.com</p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Modal.Content>
-
-        <Modal.Actions>
-        <Button
-            negative
-            icon="close"
-            labelPosition="right"
-            content="Reject"
-          />
-          <Button
-            positive
-            icon="checkmark"
-            labelPosition="right"
-            content="Accept"
-          />
-        </Modal.Actions>
-        </Modal>
+        <Container>
+          <Header as='h1' style={{paddingTop: '20px'}}>List of insurance applications</Header>
+          <Segment className="container">
+          <List divided relaxed>
+            {this.state.data.map((app, index) => (
+              <List.Item key={index}>
+              <List.Icon name="home" size="large" verticalAlign="middle" />
+              <List.Content className="listItems">
+                <List.Header as="a" onClick={()=>this.handleOnOpen(app)}>
+                  Insurance contract number: {app.numberOfApp}
+                </List.Header>
+                <List.Description as="a">Created: {app.insureData.dateStartOfAgreement}</List.Description>
+              </List.Content>
+            </List.Item>
+            ))}
+          </List>
+          </Segment>
+        </Container>
+        
+        <InsureApplicationModal data={this.state.currentApp} open={this.state.open} close={this.handleOnClose} />
       </div>
     )
   }
 }
+
+
 
 export default ModalModalExample
